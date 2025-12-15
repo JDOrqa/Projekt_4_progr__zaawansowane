c094dac2af9ac63b954947639ce1ff49b333441a
Szymon <125341558+JDOrqa@users.noreply.github.com>
2025-12-15 20:31:53 +0100

    Add new matrix operations and overloads

--- matrix.h
+++ matrix.h
@@ -18,7 +18,40 @@ public:
 	matrix(int n, int* t);
 	matrix(const matrix& m);
 	~matrix();
+matrix& alokuj(int n);
+matrix& wstaw(int x, int y, int wartosc);
+int pokaz(int x, int y) const;
 
+matrix& dowroc();
+matrix& losuj();
+matrix& losuj(int x);
+
+matrix& diagonalna(int* t);
+matrix& diagonalna_k(int k, int* t);
+
+matrix& kolumna(int x, int* t);
+matrix& wiersz(int y, int* t);
+
+matrix& przekatna();
+matrix& pod_przekatna();
+matrix& nad_przekatna();
+matrix& szachownica();
+
+matrix operator+(const matrix& m) const;
+matrix operator*(const matrix& m) const;
+
+matrix operator+(int a) const;
+matrix operator*(int a) const;
+matrix operator-(int a) const;
+
+matrix& operator++(int);
+matrix& operator--(int);
+
+matrix& operator+=(int a);
+matrix& operator-=(int a);
+matrix& operator*=(int a);
+
+matrix& operator()(double a);
 
 
 
