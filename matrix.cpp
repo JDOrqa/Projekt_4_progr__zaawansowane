9bdde170f54f66cdfb1b5ff3be8aef30fec13349
Szymon <125341558+JDOrqa@users.noreply.github.com>
2025-12-15 21:01:18 +0100

    Implement matrix constructors and methods
    Added constructors for initializing matrix with size and data.

--- matrix.cpp
+++ matrix.cpp
@@ -5,4 +5,32 @@
 
 int matrix::idx(int x, int y) const {
     return x * n + y;
+}matrix::matrix() : n(0), data(nullptr) {}
+
+matrix::matrix(int n) : n(n) {
+    data = std::make_unique<int[]>(n * n);
+    for (int i = 0; i < n * n; i++) data[i] = 0;
+}
+
+matrix::matrix(int n, int* t) : matrix(n) {
+    for (int i = 0; i < n * n; i++) data[i] = t[i];
+}
+
+matrix::matrix(const matrix& m) : matrix(m.n) {
+    for (int i = 0; i < n * n; i++) data[i] = m.data[i];
+}
+
+matrix::~matrix() {}
+
+matrix& matrix::alokuj(int n_) {
+    if (!data || n_ > n) {
+        n = n_;
+        data = std::make_unique<int[]>(n * n);
+    }
+    return *this;
+}
+
+matrix& matrix::wstaw(int x, int y, int wartosc) {
+    data[idx(x, y)] = wartosc;
+    return *this;
 }
