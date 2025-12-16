b1bf6ca2469b6ee2f8237f39b4983888526be1f0
KrzysztofDutka04 <97160815+KrzysztofDutka04@users.noreply.github.com>
2025-12-16 10:01:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,3 +2,11 @@
 #include <fstream>
 #include <vector>
 #include <iostream>
+int main() {
+    std::ifstream file("dane.txt");
+    if (!file.is_open()) {
+        std::cout << "Blad otwarcia pliku dane.txt\n";
+        return 1;
+    }
+return 0;
+}
